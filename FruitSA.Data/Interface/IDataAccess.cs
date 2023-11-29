78ae9053c1dda6434d4f5a9c83a6bf2626bd3571
Sphah <sbuthelezi2@ulwembubs.com>
2023-11-29 20:21:10 +0200

    application scaffolding / architecture

---
+++ FruitSA.Data/Interface/IDataAccess.cs
@@ -0,0 +1,15 @@
+﻿using FruitSA.Data.Module;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitSA.Data.Interface
+{
+    public interface IDataAccess
+    {
+        void CreateDatabase();
+        ConnectionContext DbContext { get; }
+    }
+}
---
+++ FruitSA.Data/Module/ConnectionContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitSA.Data.Module
+{
+    public class ConnectionContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source=dbserver;initial catalog=dbname;user id=dbuserid;password=20Azania19");
+        }
+    }
+}
